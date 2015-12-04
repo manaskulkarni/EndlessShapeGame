ca6efede12e6444d0633df588503a6923cca4f11
manaskulkarni <manas.kulkarni@digipen.edu>
2015-12-03 20:13:10 -0800

    merge with master

---
+++ EndlessShapeGame/Assets/Scripts/Behaviors/InGameBuyButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InGameBuyButton : MonoBehaviour
+{
+  public enum ButtonType
+  {
+    Revive,
+    SlowMotion,
+  }
+
+  public ButtonType type;
+  public int price;
+}
