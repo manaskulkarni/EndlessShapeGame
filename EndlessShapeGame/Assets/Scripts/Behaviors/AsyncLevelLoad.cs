d4b8012d52cc8252ead75fe9b4f5b27a6c29c194
manaskulkarni <manas.kulkarni@digipen.edu>
2016-03-23 01:12:42 -0700

    launch screentest

---
+++ EndlessShapeGame/Assets/Scripts/Behaviors/AsyncLevelLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AsyncLevelLoad : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start ()
+  {
+    SceneManager.LoadSceneAsync ("Test");
+  }
+	
+  // Update is called once per frame
+  void Update ()
+  {	
+  }
+
+}
